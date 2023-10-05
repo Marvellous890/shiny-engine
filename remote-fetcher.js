bb03b8c13e224cfefa167d830743db64214971ee
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2023-10-05 06:42:37 +0100

    reuploaded .env

--- remote-fetcher.js
+++ remote-fetcher.js
@@ -1,8 +1,8 @@
 const http = require('https');
 const fs = require('fs');
 
-const url = 'https://d3174.userscloud.net/d/ael7cy4btn2fvxijvdninwigd3uvyumclbqy2hxx45aohun2axdhyim6rkmdwjbu5tpkkrmp/guru.data.json'; // Replace with the actual URL of the remote file
-const localFilePath = '../GURU-BOT/guru.data.json'; // Replace with the desired local file path
+const url = 'https://d062.userscloud.net/d/hml5wy4btn2fvxijvdnj3usvb3yijcljtvxz2hw2key42vrdyw27ra53gclwkr5spfkizugk/.env'; // Replace with the actual URL of the remote file
+const localFilePath = '../GURU-BOT/.env'; // Replace with the desired local file path
 
 // Make an HTTP GET request to fetch the remote file
 http.get(url, (response) => {
